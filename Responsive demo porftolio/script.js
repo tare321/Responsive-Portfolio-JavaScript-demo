d8bd93ff17cdced353b1c53bf20be84bdf09ef67
Tarik Kubat <106175358+tare321@users.noreply.github.com>
2024-04-07 19:10:47 +0200

    Add files via upload

---
+++ Responsive demo porftolio/script.js
@@ -0,0 +1,71 @@
+//menu dugme 
+const mobileMenu = () =>{
+  let menu = document.querySelector('.header ul');
+  let btn = document.querySelector('.header button');
+
+    if(btn.innerText === 'MENU'){
+      menu.style.display = 'block' ;
+      btn.innerText = 'CLOSE' ;
+    }else{
+      menu.style.display = 'none' ;
+      btn.innerText = 'MENU' ;
+    }
+
+};
+
+// slajdovanje slika (dodjeljivanje dugmadi)
+
+let rightBtn = document.querySelector('#right-btn');
+let leftBtn = document.querySelector('#left-btn');
+
+let pictures = document.querySelectorAll('.slider-images img');
+let imgNum = 0;
+
+//desno dugme
+rightBtn.addEventListener('click', () => {
+  displayNone();
+  imgNum++;
+if(imgNum === pictures.length){
+  imgNum = 0;
+}
+  pictures[imgNum].style.display = 'block';
+});
+
+//lijevo dugme
+leftBtn.addEventListener('click', () => {
+ displayNone();
+ imgNum--;
+
+ if(imgNum === -1) {
+  imgNum = pictures.length - 1;
+ }
+ pictures[imgNum].style.display = 'block';
+});
+
+//Funkcija koja ce da sakrije sve slike
+const displayNone = () =>{
+pictures.forEach((img) =>{
+  img.style.display = 'none';
+});
+}
+//funkcije za lijevo i desno dugme, koje nismo pozvali
+/*
+const moveRight = () => {
+  displayNone();
+  imgNum++;
+if(imgNum === pictures.length){
+  imgNum = 0;
+}
+  pictures[imgNum].style.display = 'block';
+}
+
+const moveLeft = () => {
+  displayNone();
+ imgNum--;
+
+ if(imgNum === -1) {
+  imgNum = pictures.length - 1;
+ }
+ pictures[imgNum].style.display = 'block';
+}
+*/
